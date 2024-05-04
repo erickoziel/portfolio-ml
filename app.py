6eabf20acca1832824b337f00b50722a9adad704
erickoziel <erick.oziel@gmail.com>
2024-05-04 20:07:07 +0000

    Mi primera app

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+
+# Encabezado de la app
+st.write("""
+# Predicción de precio de casas
+Este es un estimador de precios de casas creado con un modelo de Machine Learning.
+""")
